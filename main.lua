43ecaeafa0d38b830ee648663d2bdaf0622e66d9
Micha <micha@HADES.(none)>
2012-06-22 19:03:43 +0200

    Server can now enter a starting word that gets looked up in the wiki

--- main.lua
+++ main.lua
@@ -1,56 +1,72 @@
-local connection = require("connection")
+local connection = require("Scripts/connection")
 
-lobby = require("lobby")
+lobby = require("Scripts/lobby")
 
-local wikiClient = require("wikiClient")
+wikiClient = require("Scripts/wikiClient")
 
-textBox = require("textBox")
+textBox = require("Scripts/textBox")
 plname = ""
 ipStr = ""
 
-require("misc")
-statusMsg = require("statusMsg")
+require("Scripts/misc")
+statusMsg = require("Scripts/statusMsg")
 
-menu = require("menu")
+menu = require("Scripts/menu")
 
-buttons = require("buttons")
-mainFont = love.graphics.newFont( "AveriaSans-Regular.ttf", 18 )
-buttonFont = love.graphics.newFont( "AveriaSans-Bold.ttf", 18 )
-fontHeader = love.graphics.newFont( "AveriaSans-Bold.ttf", 25 )
-fontInputHeader = love.graphics.newFont( "AveriaSans-Bold.ttf", 20 )
-fontInput = love.graphics.newFont( "AveriaSans-Regular.ttf", 16 )
-fontStatus = love.graphics.newFont( "AveriaSans-Bold.ttf", 16 )
-fontMainHeader = love.graphics.newFont( "AveriaSans-Bold.ttf",40 )
+buttons = require("Scripts/buttons")
+
+game = require("Scripts/game")
+--[[
+mainFont = love.graphics.newFont( "Fonts/AveriaSans-Regular.ttf", 18 )
+buttonFont = love.graphics.newFont( "Fonts/AveriaSans-Bold.ttf", 18 )
+fontHeader = love.graphics.newFont( "Fonts/AveriaSans-Bold.ttf", 25 )
+fontInputHeader = love.graphics.newFont( "Fonts/AveriaSans-Bold.ttf", 20 )
+fontInput = love.graphics.newFont( "Fonts/AveriaSans-Regular.ttf", 16 )
+fontStatus = love.graphics.newFont( "Fonts/AveriaSans-Bold.ttf", 16 )
+fontMainHeader = love.graphics.newFont( "Fonts/AveriaSans-Bold.ttf",40 )
+]]--
+
+mainFont = love.graphics.newFont( 18 )
+buttonFont = love.graphics.newFont( 18 )
+fontHeader = love.graphics.newFont( 25 )
+fontInputHeader = love.graphics.newFont( 20 )
+fontInput = love.graphics.newFont( 16 )
+fontStatus = love.graphics.newFont( 16 )
+fontMainHeader = love.graphics.newFont( 40 )
 
 colMainBg = { r=230, g=205, b=174 }
 colBg = { r=149, g=124, b=101 }
 colLobby = { r=241, g=229, b=209 }
+colTextInput = { r=255, g=249, b=229 }
 
 plName = ""
 
 PORT = 3456
 
-local inputStr = ""
-local wordToSearchFor = ""
-local inputNum = 0
-
-local numberOfChoices = 5
-
-local foundStartPage = false
 
 local pageSource = ""
 
 server = nil		-- server object. When not nil, then connection is established.
 client = nil		-- client object. When not nil, then connection is established.
 
+
 function love.load(arg)
 	menu.initMainMenu(buttons)
 	love.keyboard.setKeyRepeat( 0.3, 0.03 )	
 	--wikiClient.newWord()
-
-	tb = textBox.new(10, 220, 5, fontInput, 200)
-	textBox.setAccess( tb, true )
-	textBox.setColour( tb, 30, 0, 0, 128 )
+	--wikiClient.nextWord()
+	titleStr = ""
+	fullTitle = "schrödinger"
+	buttonFont:getWidth( fullTitle )
+	j = 0
+	while buttonFont:getWidth( titleStr ) < buttonWidth-20 and #titleStr < #fullTitle do
+		print(titleStr.. " " .. buttonFont:getWidth( titleStr ))
+		titleStr = fullTitle:sub( 1, j )
+		print("new: " .. titleStr)
+		j = j+1
+	end
+	--tb = textBox.new( 10, 10, 5, fontInput, 25)
+	--textBox.setAccess ( tb , true )
 end
 
 local lastSent = os.time()
@@ -59,15 +75,13 @@ local msg
 nextFrameEvent = {}			-- these functions will be called after the next draw call.
 
 function love.update()
-	
 	if server then
 		connection.runServer( server )
 	elseif client then
 		connection.runClient( client )
 	end
 	-- go through list of functions in nextFrameEvent and if they have waited the set amount of frames, call them.
-	-- This is needed to make sure client connection does not disable status messages, which would otherwise not show
-	-- (especially the "attempting to connect" message)
+	-- This is needed to make sure client connection does not disable status messages, which would otherwise not show (especially the "attempting to connect" message)
 	for k, v in pairs( nextFrameEvent ) do	
 		v.frames = v.frames - 1
 		if v.frames <= 0 then
@@ -82,10 +96,12 @@ function love.draw()
 		menu.showMainMenu()
 	elseif lobby.active() then
 		lobby.showPlayers()
+	elseif game.active() then
+		game.show()
 	end
-	statusMsg.display()
 	textBox.display()
 	buttons.show()
+	statusMsg.display()
 end
 
 function love.keypressed(key, unicode)
@@ -95,6 +111,7 @@ end
 function love.mousepressed()
 	menu.handleClick()
 end
+
 
 function startServer()
 	server = connection.initServer( "localhost", PORT, 10 )
@@ -112,10 +129,6 @@ function startClient()
 	end
 end
 
-function startGame()
-	statusMsg.new("Game starting.")
-	print("game Started")
-end
 
 function love.quit()
 	if server then
